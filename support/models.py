7cb2d44ae15f048254f514ae91ce666f9b18bdc8
Holly Ann Coloma <holly.life.15@gmail.com>
2024-06-30 14:49:20 +0000

    Create and connect database

--- support/models.py
+++ support/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Profile Model
+class Profile(models.Model):
+    ROLE_OPTIONS = (
+        ('Admin', 'Admin'),
+        ('User', 'User'),
+        ('Tech Support', 'Tech Support'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_OPTIONS, default='User')
+
+    def __str__(self):
+        return self.user.username
--- tech_tackles/settings.py
+++ tech_tackles/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=cas&h&)6oj1y3216el9dt4qqy000$n(^ryfvyukena=b2#-$#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-hpcoloma-pp4techtackles-402lirmizz0.ws.codeinstitute-ide.net',
@@ -75,12 +80,17 @@ WSGI_APPLICATION = 'tech_tackles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation

c8b971e7c42c6103945fed19a7989b60852e78f1
Holly Ann Coloma <holly.life.15@gmail.com>
2024-06-30 21:43:36 +0000

    Create knowledge base and comment models, added to admin access

--- support/admin.py
+++ support/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Ticket
+from .models import Profile, Ticket, KnowledgeBase, Comment
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Ticket)
+admin.site.register(KnowledgeBase)
+admin.site.register(Comment)
--- support/models.py
+++ support/models.py
@@ -16,6 +16,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Ticket(models.Model):
     STATUS_OPTIONS = (
         ('Open', 'Open'),
@@ -30,7 +31,35 @@ class Ticket(models.Model):
     description = models.TextField()
     status = models.CharField(max_length=20, choices=STATUS_OPTIONS, default='Open')
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if not self.ticket_id:
+            now = datetime.now()
+            # Generate ticket+_id as TT-YYYYMMDD-NNN(NNN is a three digit number)
+            today_tickets_count = Ticket.objectsfilter(created_at__date=now.date()).count() + 1
+            self.ticket_id = f"TT-{now.year}-{now.month:02d}{now.day:02d}-{today_tickets_count:03d}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.ticket_id
+
+
+class KnowledgeBase(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    knowledge_base = models.ForeignKey(KnowledgeBase, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment

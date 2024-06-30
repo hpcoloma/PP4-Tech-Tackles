d63af0b36248d37f1411395db471ed8ef2498942
Holly Ann Coloma <holly.life.15@gmail.com>
2024-06-30 16:14:30 +0000

    Create Profile and Ticket models, add CSRF on settings

--- support/models.py
+++ support/models.py
@@ -15,3 +15,22 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Ticket(models.Model):
+    STATUS_OPTIONS = (
+        ('Open', 'Open'),
+        ('In Progress', 'In Progress'),
+        ('Closed', 'Closed'),
+    )
+
+    ticket_id = models.CharField(max_length=20, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
+    tech_support = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_tickets")
+    subject = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_OPTIONS, default='Open')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.ticket_id

574a76f2b9b53a0190ae7aeb1c5f1ec3dce362a3
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-01 21:02:07 +0000

    Add register, submit ticket and view my ticket views

--- support/views.py
+++ support/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .models import Profile, Ticket, KnowledgeBase, Comment
+from django.contrib.auth.models import User
 
 
 
@@ -16,3 +20,21 @@ def register(request):
     else:
         form = UserForm()
     return render(request, 'support/register.html', {'form': form})
+
+@login_required
+def submit_ticket(request):
+    if request.method == "POST":
+        form = TicketForm(request.POST)
+        if form.is_valid()
+            ticket = form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            return redirect('view_my_tickets')
+    else:
+        form = TicketForm()
+    return render(request, 'support/submit_ticket.html', {'form': form})
+
+@login_required
+def view_my_tickets(request):
+    tickets = Ticket.objects.filter(user=request.user)
+    return render(request, 'support/view_my_tickets.html', {'tickets': tickets})

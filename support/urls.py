249f54e38fe51fbebebff8e1751573dc84e92255
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-01 22:50:39 +0000

    Add delete ticket, add comment, edit ticket, update ticket status and view all ticket templates and views, update urlpatterns

--- support/urls.py
+++ support/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('submit_ticket/', views.submit_ticket, name='submit_ticket'),
     path('view_my_tickets/', views.view_my_tickets, name='view_my_tickets'),
+    path('edit_ticket/<int:ticket_id>/', views.edit_ticket, name='edit_ticket'),
+    path('delete_ticket/<int:ticket_id>/', views.delete_ticket, name='delete_ticket'),
+    path('add_comment/<int:ticket_id>/', views.add_comment, name='add_comment'),
+    path('view_all_tickets/<int:ticket_id>/', views.view_all_tickets, name='view_all_tickets'),
+    path('update_ticket_status/<int:ticket_id>/', views.update_ticket_status, name='update_ticket_status'),
 ]
--- support/views.py
+++ support/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .models import Profile, Ticket, KnowledgeBase, Comment
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
 
@@ -59,19 +59,35 @@ def delete_ticket(request, ticket_id):
         return redirect('view_my_tickets')
     return render(request, 'support/delete_ticket.html', {'ticket': ticket})
 
+@login_required
+def add_comment(request, ticket_id):
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.ticket = ticket
+            comment.save()
+            return redirect('view_ticket', ticket_id=ticket.id)
+    else:
+        form = CommentForm()
+    return render(request, 'support/add_comment.html', {'form': form})
 
+@login_required
+def view_all_tickets(request):
+    if request.user.profile.role in ['Admin', 'Tech Support']:
+        tickets = Tickets.object.all()
+        return render(request, 'support/view_all_tickets.html', {'tickets': tickets})
+    return redirect('home')
 
-
-
-
-
-
-    # if request.method == 'POST':
-    #     form = CommentForm(request.POST)
-    #     if form.is_valid():
-    #         comment = form.save(commit=False)
-    #         comment.user = request.user
-    #         comment.ticket = ticket
-    #         comment.save()
-    #         return redirect('view_ticket')
-
+@login_required
+def update_ticket_status(request, ticket_id):
+    if request.user.profile.role in ['Admin', 'Tech Support']:
+        ticket = get_object_or_404(Ticket, pk=ticket_id)
+        if request.method == 'POST':
+            ticket.status = request.POST.get('status')
+            ticket.save()
+            return redirect('view_all_tickets')
+        return render(request, 'support/update_ticket_status.html', {'ticket': ticket})
+    return redirect('home')

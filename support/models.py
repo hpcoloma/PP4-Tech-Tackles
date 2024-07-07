96003eee1090b1c4fc51d98542e1d258af4a59f0
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-07 19:01:21 +0000

    Add comment model

--- support/models.py
+++ support/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# User profile model
+ROLE_OPTIONS = (
+    ('admin', 'Admin'),
+    ('user', 'User'),
+    ('tech_support', 'Tech Support'),  
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    surname = models.CharField(max_length=50, null=False, blank=False)
+    role = models.CharField(max_length=20, choices=ROLE_OPTIONS, default='user')
+
+def __str__(self):
+    return f'{self.user.username} Profile'
+
 class Ticket(models.Model):
     STATUS_OPTIONS = (
         ('Open', 'Open'),
@@ -16,3 +32,15 @@ class Ticket(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_OPTIONS, default='Open')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.ticket_id} - {self.subject} by {self.user}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter") # Many to one
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='comments', null=True, blank=True) # Many to one
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ticket_id} - {self.ticket} by {self.user}"
--- techtackles/settings.py
+++ techtackles/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-hpcoloma-pp4techtackles-wk0blf6r593.ws.codeinstitute-ide.net',
 '.herokuapp.com']

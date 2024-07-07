1a9032f9be388e4b9fbe5e59be5f52315c61b670
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-07 15:13:29 +0000

    Add super user, add ticket model

--- support/models.py
+++ support/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Ticket(models.Model):
+    STATUS_OPTIONS = (
+        ('Open', 'Open'),
+        ('In Progress', 'In Progress'),
+        ('Closed', 'Closed'),
+    )
+
+    ticket_id = models.CharField(max_length=20, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_OPTIONS, default='Open')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
--- techtackles/settings.py
+++ techtackles/settings.py
@@ -91,6 +91,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.codeinstitute-ide.net",
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 

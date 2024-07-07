c5e2aa8f70bc387ac885bf91074ac37546af3162
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-07 21:43:32 +0000

    Update Ticket List view by adding paginated list, style, add base and index templates

--- support/views.py
+++ support/views.py
@@ -5,9 +5,10 @@ from .models import Ticket
 
 # Create your views here.
 class TicketList(generic.ListView):
-    model = Ticket
-    # queryset = Ticket.objects.filter(status='Closed')
-    # template_name = "ticket_list.html"
+    # model = Ticket
+    queryset = Ticket.objects.all()
+    template_name = "support/index.html"
+    paginated_by = 5
     
 
 

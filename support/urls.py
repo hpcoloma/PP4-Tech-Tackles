aa7a102a0fa49f8b897b8687651b77f9b36288ab
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-09 20:32:39 +0000

    Add comment form and add comment views

--- support/urls.py
+++ support/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
-from support.views import home_page, TicketListView, TicketDetailView
+from support.views import home_page, TicketListView, TicketDetailView, add_comment
 
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('tickets/', TicketListView.as_view(), name='ticket_list'),
     path('ticket/<int:pk>/', TicketDetailView.as_view(), name='ticket_detail'),
+    path('ticket/<int:pk>/comment/', add_comment, name='add_comment'),
 ]
 
 
--- support/views.py
+++ support/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Ticket
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from .forms import CommentForm
 
 
 
@@ -24,6 +26,21 @@ class TicketListView(LoginRequiredMixin, ListView):
 class TicketDetailView(LoginRequiredMixin, DetailView):
     model = Ticket
     template_name = 'support/ticket_detail.html'
+
+@login_required
+def add_comment(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.ticket = ticket
+            comment.user = request.user
+            comment.save()
+            return redirect('ticket_detail', pk=ticket.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'support/add_comment.html', {'form': form})
 
     
     

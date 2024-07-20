40c3cf419bba2f94b1d4457687fb0abcc0372846
Holly Ann Coloma <holly.life.15@gmail.com>
2024-07-20 08:02:02 +0000

    Add tests for Comment and Ticket Forms

--- support/tests_forms.py
+++ support/tests_forms.py
@@ -1,9 +1,26 @@
 from django.test import TestCase
-from .forms import CommentForm
+from .forms import CommentForm, TicketForm
 
+class CommentFormTest(TestCase):
 
-class TestCommentForm(TestCase):
+    def test_valid_comment_form(self):
+        form_data = {'comment': 'This is a comment'}
+        form = CommentForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
-    def test_form_is_valid(self):
-        comment_form = CommentForm({'body': 'This is a great post'})
-        self.assertTrue(comment_form.is_valid())
+    def test_invalid_comment_form(self):
+        form_data = {'comment': ''}
+        form = CommentForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+class TicketFormTest(TestCase):
+
+    def test_valid_ticket_form(self):
+        form_data = {'subject': 'Test Subject', 'description': 'Test description'}
+        form = TicketForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_ticket_form(self):
+        form_data = {'subject': '', 'description': 'Test description'}
+        form = TicketForm(data=form_data)
+        self.assertFalse(form.is_valid())
